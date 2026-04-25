b0e21de101d91f9e1b890d1942811858dcca1bd7
mie-intel <paryapradhanika@gmail.com>
2026-04-25 18:09:18 +0700

    fix

--- apps/app/(game)/casual/page.tsx
+++ apps/app/(game)/casual/page.tsx
@@ -10,13 +10,8 @@ export default function CasualPage() {
           <Spinner size="lg" />
         </div>
       }
-    import React, { Suspense } from 'react';
-    import CasualClient from './CasualClient';
-
-    export default function CasualPage() {
-      return (
-        <Suspense fallback={<div className="flex-1 flex items-center justify-center"><div>Loading...</div></div>}>
-          <CasualClient />
-        </Suspense>
-      );
-    }
+    >
+      <CasualClient />
+    </Suspense>
+  );
+}
